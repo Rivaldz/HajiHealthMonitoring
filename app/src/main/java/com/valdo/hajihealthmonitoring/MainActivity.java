ccf15ed6f67e967dfbf4ae4c107dca6e75031fe3
AdmajaRois <rois.da.silva@gmail.com>
2020-01-06 14:24:00 +0700

    bottom navigasi done

--- app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
+++ app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
@@ -2,11 +2,14 @@ package com.valdo.hajihealthmonitoring;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.valdo.hajihealthmonitoring.fragment.HomeFragment;
+import com.valdo.hajihealthmonitoring.fragment.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -14,11 +17,35 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigasi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment !=null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.action_home:
+                fragment =new HomeFragment();
+                break;
+            case R.id.action_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 
     @Override
