2815bf91991915842886d0a5ed8f553f48bfb424
AdmajaRois <rois.da.silva@gmail.com>
2020-01-06 14:10:04 +0700

    implementasi bottomnavigasi ke mainactivity

--- app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
+++ app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
@@ -1,14 +1,28 @@
 package com.valdo.hajihealthmonitoring;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
