27e863841c9c1a1cd31b379f86372cf473d4200a
Rivaldz <rivalzsetyo@gmail.com>
2020-01-08 15:37:35 +0700

    add click listener button

--- app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
+++ app/src/main/java/com/valdo/hajihealthmonitoring/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.valdo.hajihealthmonitoring.fragment.HomeFragment;
 import com.valdo.hajihealthmonitoring.fragment.ProfileFragment;
--- app/src/main/java/com/valdo/hajihealthmonitoring/activity/LoginActivity.java
+++ app/src/main/java/com/valdo/hajihealthmonitoring/activity/LoginActivity.java
@@ -2,15 +2,46 @@ package com.valdo.hajihealthmonitoring.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.valdo.hajihealthmonitoring.MainActivity;
 import com.valdo.hajihealthmonitoring.R;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView register;
+    EditText email, password;
+    Button loginBut ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register = findViewById(R.id.textRegister);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        loginBut = findViewById(R.id.buttonLogin);
+
+       register.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+
+           }
+       });
+
+       loginBut.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(LoginActivity.this, MainActivity.class));
+           }
+       });
+
+
     }
 }
