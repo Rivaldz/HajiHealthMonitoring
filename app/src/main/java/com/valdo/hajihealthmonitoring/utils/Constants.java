1fb350de07376e5f83c461cd24eaa7b4c560faff
AdmajaRois <rois.da.silva@gmail.com>
2020-01-24 10:57:59 +0700

    menambahkan constanta untuk berita

---
+++ app/src/main/java/com/valdo/hajihealthmonitoring/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.valdo.hajihealthmonitoring.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Constants {
+    private Constants() {
+
+    }
+    public static String API_KEY = "59309fa6619942b8ba20256bb34b0e0a";
+    public static String NEWS_SOURCE = "id";
+    public static ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DESCRIPTION= "description";
+    public static final String KEY_URL = "url";
+    public static final String KEY_URLTOIMAGE = "urlToImage";
+    public static final String KEY_PUBLISHEDAT = "publishedAt";
+}
